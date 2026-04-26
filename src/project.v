97121cc56153e85bd69e15287757a7f1c784bcbc
jose <bustamanteg@gmail.com>
2026-04-26 16:38:34 -0500

    project de edu

--- src/project.v
+++ src/project.v
@@ -1,34 +1,38 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
+  // Reset activo en bajo → lo invertimos
+  wire rst = ~rst_n;
 
-wire rst=~rst_n;
-wire [3:0] count;
-contador my_counter(.clk(clk),.rst(rst),.count(count));
+  // Cable conecta la instancia al los terminales
+  wire [3:0] count;
 
+  // INSTANCIA DEL CONTADOR
+  contador my_counter (
+      .clk(clk),
+      .rst(rst),
+      .count(count)
+  );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out[3:0]=count;
-  assign uo_out[7:4]=0;
-  assign uio_out=0;
-  assign uio_oe=0;
+  // Salidas hacia los pines del chip
+  assign uo_out[3:0] = count;
+  assign uo_out[7:4] = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  // No usamos IOs bidireccionales
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // Evitar warnings
+  wire _unused = &{ena, ui_in, uio_in, 1'b0};
 
 endmodule
